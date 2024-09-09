70a281e532eef9b5c018c37f46cae569010a188b
Anton Johansson <anton9797johansson@hotmail.com>
2024-09-09 17:47:15 +0200

    wip join room

--- src/assets/components/room/SelectedRoom.tsx
+++ src/assets/components/room/SelectedRoom.tsx
@@ -1,9 +1,37 @@
 import { useEffect, useState } from "react";
 import RoomInterface from "../interface/RoomInterface";
+import { jwtDecode } from "jwt-decode";
 
 function SelectedRoom({ roomId }: { roomId: string }) {
   const [selectedRoom, setSelectedRoom] =
     useState<RoomInterface | null>(null);
+    const [loggedInUserId, setLoggedInUserId] = useState<string | null>(null);
+
+    useEffect(() => {
+      const token = localStorage.getItem("token");
+      if (token) {
+        const decodedToken = jwtDecode<{ sub: string }>(token);
+        setLoggedInUserId(decodedToken.sub); 
+      }
+    }, []);
+
+    function joinRoom() {
+      fetch("http://localhost:8080/user/get-user-from-username/" + loggedInUserId)
+      .then(res => res.json())
+      .then(data => {
+        console.log(selectedRoom);
+        
+         fetch("http://localhost:8080/room/join/" + selectedRoom?.roomId + "/" + data.userId, {
+          method: "POST"
+         })
+         .then(res => res.json())
+         .then(data => {
+          console.log(data);
+          
+         })
+        
+      })
+    }
 
   useEffect(() => {
     fetch(`http://localhost:8080/room/${roomId}`)
@@ -20,6 +48,7 @@ function SelectedRoom({ roomId }: { roomId: string }) {
     <>
       <div key={selectedRoom.roomId}>
         <h2>{selectedRoom.roomName}</h2>
+        <button onClick={joinRoom}>Gå med</button>
         <strong>
           <p>
             <u>Deltagare:</u>
